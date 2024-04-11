45c50501bd7173e115d7d527cd1a57d36d5e8fca
Your Name <you@example.com>
2024-04-12 02:43:10 +0300

    all

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="navigation">
-    <nuxt-link to="/" class="link"> домой</nuxt-link>
-    <div class="container">
+    <div class="flex link">
+      <img class="h-[30px] w-[30px] rounded-[30px] mr-[10px]" src="/4.jpg" alt=""/> 
+      <nuxt-link to="/" > Домой</nuxt-link>
+    </div>
+    <div class="mr-[50px]">
 
-      <nuxt-link to="/about_us" class="link">о нас</nuxt-link>
+      <nuxt-link to="/about_us" class="link">О компании</nuxt-link>
 
-      <nuxt-link to="product" class="link">продукты</nuxt-link>
+      <nuxt-link to="product" class="link">Продукты</nuxt-link>
 
-      <nuxt-link to="settings" class="link">настройки</nuxt-link>
+      <nuxt-link to="settings" class="link">Обучение</nuxt-link>
     </div>
   </div>
 
@@ -23,9 +26,11 @@
 .link {
   border-width: 2px;
   margin-left: 50px;
+  border-radius: 20px;
+  padding: 5px 10px 5px 10px;
+  border-color: black;
+  background-color:aqua
 }
 
-.container {
 
-}
 </style>
