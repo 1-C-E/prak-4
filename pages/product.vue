df9791d1fe03b8047ed57a3b9687c0a97f7c9b2e
1-C-E <pitenevv@mail.ru>
2024-04-11 23:39:54 +0300

    some changes

---
+++ pages/product.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Navbar from "~/components/navbar.vue";
+</script>
+
+
+<template>
+  <div>
+    <navbar/>
+    <div class="text">прукдты</div>
+  </div>
+</template>
+
+
+<style>
+.text {
+  font-size: 70px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+}
+</style>
