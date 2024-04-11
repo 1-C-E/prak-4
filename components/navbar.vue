8b190287832646c11034b5e6e5cc953c67509c98
1-C-E <pitenevv@mail.ru>
2024-04-11 23:30:01 +0300

    first commit

---
+++ components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navigation">
+    <nuxt-link to="/" class="link"> домоооооой</nuxt-link>
+    <div class="container">
+
+      <nuxt-link to="/about_us" class="link">онас</nuxt-link>
+
+      <nuxt-link to="product" class="link">прукдты</nuxt-link>
+
+      <nuxt-link to="settings" class="link">настойки</nuxt-link>
+    </div>
+  </div>
+
+</template>
+
+<style>
+.navigation {
+  display: flex;
+  justify-content: space-between;
+}
+.link {
+  border-width: 2px;
+  margin-left: 50px;
+}
+
+.container {
+
+}
+</style>
---
+++ pages/about_us.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Navbar from "~/components/navbar.vue";
+</script>
+
+
+<template>
+  <div>
+    <navbar/>
+    <div >
+      <div class="text">о на с</div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.text {
+  display: flex;
+  font-size: 70px;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+</style>
